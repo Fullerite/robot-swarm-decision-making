808ce9bc529c7cecca24fb5931704b892609d262
Fullerite <sh.ramil2005@gmail.com>
2025-04-19 22:09:27 +0300

    feat(testing): add shell script for automated initialization of the swarm

---
+++ launch_swarm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SWARM_SIZE=30
+
+PROPOSALS=("Go Left" "Go Right" "Stay Put" "Go Forward")
+
+for (( i=1; i<=SWARM_SIZE; i++ ))
+do
+  PROPOSAL=${PROPOSALS[$((RANDOM % ${#PROPOSALS[@]}))]}
+  ROBOT_ID="R$i"
+
+  echo "Launching Robot $ROBOT_ID with proposal '$PROPOSAL'"
+
+  python3 robot.py --robot-id "$ROBOT_ID" --proposal "$PROPOSAL" --swarm-size "$SWARM_SIZE" &
+done
+
+wait
+
+echo "All robots have finished."
